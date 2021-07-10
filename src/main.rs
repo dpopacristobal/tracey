e6406ef0b4ad98ef16175ddeb4d053d0b54cce85
Daniel Popa <dpopacristobal@gmail.com>
2021-07-10 18:57:16 +0200

    Improve CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,56 @@ use rey_skytracer::materials::Lambertian;
 use rey_skytracer::render::render;
 use rey_skytracer::scene::{get_cornell_box_scene, Scene};
 
-use clap::Clap;
+use structopt::StructOpt;
 
-/// Simple CLI to showcase Tracey's ray tracing
-#[derive(Clap)]
-#[clap(name = "Tracey")]
+#[derive(StructOpt)]
+#[structopt(
+    name = "Tracey",
+    about = "A simple CLI to show the ray tracing capabilities in Tracey."
+)]
 struct TraceyArgs {
     /// Width of the output image in pixels
-    #[clap(short, long)]
+    #[structopt(long)]
     width: u32,
 
     /// Number of rays used per pixel
-    #[clap(short, long)]
+    #[structopt(long)]
     rays: u32,
+
+    /// Path to mesh that will be put into a Cornell box scene
+    #[structopt(long, conflicts_with = "sample-scene")]
+    mesh_path: Option<String>,
+
+    /// Sample scene number
+    #[structopt(long, conflicts_with = "mesh-path")]
+    sample_scene: Option<u32>,
 }
 
 fn main() {
-    let args = TraceyArgs::parse();
-
-    let blue_mat = Arc::new(Lambertian::new(Color::new(0.45, 0.71, 0.95)));
-    // let metal_mat = Arc::new(Metal::new(Color::new(0.45, 0.71, 0.95), 0.2));
-
-    let mut objects = World::default();
-    let triangle_mesh_opt = load_mesh(Path::new("./sample_meshes/tachikoma_3.obj"), blue_mat);
-    objects.add(Arc::new(triangle_mesh_opt.unwrap()));
-
-    let scene = get_cornell_box_scene(objects);
+    let args = TraceyArgs::from_args();
+    let scene = scene_from_args(args.mesh_path, args.sample_scene);
     render(args.width, args.rays, scene);
+}
+
+fn scene_from_args(mesh_path: Option<String>, sample_scene: Option<u32>) -> Scene {
+    // let metal_mat = Arc::new(Metal::new(Color::new(0.45, 0.71, 0.95), 0.2));
+    if let Some(mesh_path) = mesh_path {
+        let grey_mat = Arc::new(Lambertian::new(Color::new(0.25, 0.25, 0.25)));
+        let mut objects = World::default();
+        let triangle_mesh_opt = load_mesh(Path::new(&mesh_path), grey_mat);
+        objects.add(Arc::new(triangle_mesh_opt.unwrap()));
+        get_cornell_box_scene(objects)
+    } else {
+        match sample_scene.unwrap() {
+            0 => {
+                let blue_mat = Arc::new(Lambertian::new(Color::new(0.45, 0.71, 0.95)));
+                let mut objects = World::default();
+                let triangle_mesh_opt =
+                    load_mesh(Path::new("./sample_meshes/tachikoma_3.obj"), blue_mat);
+                objects.add(Arc::new(triangle_mesh_opt.unwrap()));
+                get_cornell_box_scene(objects)
+            }
+            _ => panic!("Invalid sample-scene value used. Use one of [0]"),
+        }
+    }
 }
